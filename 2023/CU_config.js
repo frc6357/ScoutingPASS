ecdcfc768b870c0414f32749a04d155b534e1bef
Rushil_Ram <potatobervagesalad@gmail.com>
2023-03-06 12:14:54 -0600

    Test Event

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023txbel",
+      "defaultValue": "2022txdri",
       "required": "true",
       "disabled": "true"
     },
