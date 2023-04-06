d92f512c47b9051ec56a1d9bb46277214cb99e60
Rushil_Ram <potatobervagesalad@gmail.com>
2023-04-06 13:51:57 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023txcle",
+      "defaultValue": "2023txcmp2",
       "required": "true",
       "disabled": "true"
     },
